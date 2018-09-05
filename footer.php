2cfb7119721c1bb15d4e6c4ba52670d877c22880
Brandon <brandonbland90@gmail.com>
2018-09-05 14:21:02 -0400

    Added css/js files, made index, header, footer files dynamic

---
+++ footer.php
@@ -0,0 +1,43 @@
+        </div>
+
+          <!-- Sidebar widgets -->
+          <div class="col-md-3 offset-md-1 bs-widgets">
+
+            <div class="bs-widget">
+              <h3>Description</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, veritatis autem, aspernatur sed excepturi consectetur dolorum ipsam nisi quis explicabo sunt quod perspiciatis pariatur quam nostrum. Est deserunt, unde quod!</p>
+            </div>
+
+            <div class="bs-widget">
+              <h3>Recent Posts</h3>
+              <ul class="list-unstyled">
+                <li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+                <li><a href="#">Repellendus, mollitia reiciendis fuga id.</a></li>
+                <li><a href="#">Sapiente excepturi voluptatum ex odio.</a></li>
+                <li><a href="#">Corporis, commodi, similique! Reprehenderit, modi.</a></li>
+                <li><a href="#">Earum facere voluptatem quo odit?</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <hr>
+
+      </div> <!-- /container -->
+
+    </main>
+
+    <footer class="container">
+      <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('title'); ?></p>
+    </footer>
+
+    <?php wp_footer(); ?>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="<?php bloginfo('template_url') ?>/js/popper.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,1 +1,15 @@
- 
+<?php get_header(); ?>
+
+
+
+            <!-- Post snippet -->
+            <div class="bs-post-snippet">
+              <h2><a href="#">Blog Post Title</a></h2>
+              <p><small>By <a href="#">Author</a>, Dec 25, 2007</small></p>
+
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus quas aliquam recusandae minima quisquam qui vero perferendis consequatur cumque libero quis ut magni, est, dolor inventore quos, incidunt repellendus voluptate, error exercitationem atque quidem ea possimus officia! Harum iusto, ipsa.
+                <a href="#">&nbsp;...Read More</a>
+              </p>
+            </div>
+
+<?php get_footer(); ?>
